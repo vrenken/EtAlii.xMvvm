2ee70ae5ab0d1e095af9d96e85fbef508d67a863
Peter Vrenken <github.com@petervrenken.nl>
2020-02-26 16:14:02 +0100

    Added simple string.Replace based templates for both the generated and partial class.

---
+++ Playground/1. Structure/xMvvm.Unity/Assets/_Tests/XamlVariant1/LoginView.v1xaml.cs
@@ -0,0 +1,10 @@
+namespace ROOT_NAMESPACE
+{
+    using System;
+    using System.IO;
+    using UnityEngine;
+
+    public partial class LoginView
+    {
+    }
+}
--- Playground/1. Structure/xMvvm.Unity/Assets/xMvvm/Editor/XamlVariant1/Variant1ViewCodeManager.cs
+++ Playground/1. Structure/xMvvm.Unity/Assets/xMvvm/Editor/XamlVariant1/Variant1ViewCodeManager.cs
@@ -7,20 +7,78 @@
     public class Variant1ViewCodeManager : IViewCodeManager
     {
         private const string XamlFileExtension = ".v1xaml";
+        private const string GeneratedCsFileExtension = ".v1xaml.g.cs";
+        private const string PartialCsFileExtension = ".v1xaml.cs";
 
         public bool CanManage(string asset) => string.Compare(Path.GetExtension(asset), XamlFileExtension, StringComparison.OrdinalIgnoreCase) == 0;
 
+        private readonly string _generatedCsTemplate;
+        private readonly string _partialCsTemplate;
+        
+        public Variant1ViewCodeManager()
+        {
+            var generatedCsTemplateFileName = Path.Combine(Application.dataPath, "xMvvm/Editor/XamlVariant1", "GeneratedCsTemplate.txt");
+            _generatedCsTemplate = File.ReadAllText(generatedCsTemplateFileName);
+
+            var partialCsTemplateFileName = Path.Combine(Application.dataPath, "xMvvm/Editor/XamlVariant1", "PartialCsTemplate.txt");
+            _partialCsTemplate = File.ReadAllText(partialCsTemplateFileName);
+        }
+        
         public void Delete(string asset)
         {
+            BuildRelevantFileNames(asset, out _, out var fullGeneratedFileName, out _);
+
+            // We always delete the generated file and recreate it.
+            if (File.Exists(fullGeneratedFileName))
+            {
+                File.Delete(fullGeneratedFileName);
+            }
         }
 
         public void Create(string asset)
         {
-            var fullFilename = Path.Combine(Application.dataPath, "..", asset);
-                
-            var content = File.ReadAllText(fullFilename);
+            BuildRelevantFileNames(asset, out var filename, out var generatedFileName, out var partialFileName);
+            var name = Path.GetFileNameWithoutExtension(asset);
+
+            var xamlContent = File.ReadAllText(filename);
             
             // Debug.Log(content);
+
+            CreateGeneratedFile(name, generatedFileName);
+
+            CreatePartialFile(name, partialFileName);
+        }
+
+        private void CreatePartialFile(string name, string partialFileName)
+        {
+            // We only generated the partial file when it doesn't exist yet.
+            if (File.Exists(partialFileName)) return;
+            
+            var partialCsContent = _partialCsTemplate.Replace("CLASS", name); 
+            File.WriteAllText(partialFileName, partialCsContent);
+        }
+        
+        private void CreateGeneratedFile(string name, string generatedFileName)
+        {
+            // We always delete the generated file and recreate it.
+            if (File.Exists(generatedFileName))
+            {
+                File.Delete(generatedFileName);
+            }
+
+            var generatedCsContent = _generatedCsTemplate.Replace("CLASS", name); 
+            File.WriteAllText(generatedFileName, generatedCsContent);
+        }
+        
+        private void BuildRelevantFileNames(
+            string asset, 
+            out string fileName, 
+            out string generatedFileName,
+            out string partialFileName)
+        {
+            fileName = Path.Combine(Application.dataPath, "..", asset);
+            generatedFileName = Path.ChangeExtension(fileName, GeneratedCsFileExtension);
+            partialFileName = Path.ChangeExtension(fileName, PartialCsFileExtension);
         }
     }
 }
