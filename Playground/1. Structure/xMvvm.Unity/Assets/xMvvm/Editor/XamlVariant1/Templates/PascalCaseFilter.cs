db4179bbf851ef233c94df92cfc36a2d72243396
Peter Vrenken <github.com@petervrenken.nl>
2020-02-28 00:06:41 +0100

    Improved templates

---
+++ Playground/1. Structure/xMvvm.Unity/Assets/xMvvm/Editor/XamlVariant1/Templates/PascalCaseFilter.cs
@@ -0,0 +1,20 @@
+﻿namespace EtAlii.xMvvm
+{
+    public static class PascalCaseFilter
+    {
+        public static string PascalCase(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
+
+            var characters = input.ToCharArray();
+            characters[0] = char.ToLower(characters[0]);
+
+            return new string(characters);
+        }
+        
+        public static string PrivateMember(string input) => "_" + PascalCase(input);
+    }
+}
