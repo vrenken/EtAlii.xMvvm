e002a6a48be7122148e4bef4303588a53c692662
Peter Vrenken <github.com@petervrenken.nl>
2020-02-28 00:27:52 +0100

    Further implementation of the Binding principles.

--- Playground/1. Structure/xMvvm.Unity/Assets/xMvvm/Editor/XamlVariant1/_Model/Binding.cs
+++ Playground/1. Structure/xMvvm.Unity/Assets/xMvvm/Editor/XamlVariant1/_Model/Binding.cs
@@ -4,10 +4,18 @@ namespace EtAlii.xMvvm
 
     [LiquidType( new []
     {
-        nameof(Name)
+        nameof(Name),
+        nameof(Component),
+        nameof(ComponentProperty),
+        nameof(ViewModelProperty),
+        nameof(Mode)
     })]
     public class Binding
     {
         public string Name { get; set; }
+        public string Component { get; set; }
+        public string ComponentProperty { get; set; }
+        public string ViewModelProperty { get; set; }
+        public string Mode { get; set; }
     }
 }
--- Playground/1. Structure/xMvvm.Unity/Assets/xMvvm/Runtime/XamlVariant1/Binding.cs
+++ Playground/1. Structure/xMvvm.Unity/Assets/xMvvm/Runtime/XamlVariant1/Binding.cs
@@ -2,5 +2,9 @@ namespace EtAlii.xMvvm.XamlVariant1
 {
     public class Binding
     {
+        public Binding(string component, string componentProperty, string viewModelProperty)
+        {
+            
+        }
     }
 }
