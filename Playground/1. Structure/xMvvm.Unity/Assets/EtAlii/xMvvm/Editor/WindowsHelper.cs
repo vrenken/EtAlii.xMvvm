870de0425dcb8b1321b5b54a1bb68d5951590cc3
Peter Vrenken <github.com@petervrenken.nl>
2020-03-17 17:04:23 +0100

    Added WindowsHelper

---
+++ Playground/1. Structure/xMvvm.Unity/Assets/EtAlii/xMvvm/Editor/WindowsHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace EtAlii.xMvvm
+{
+    using UnityEditor;
+
+    public class WindowsHelper 
+    {
+        public static void GiveConsoleWindowFocus()
+        {
+            var consoleWindowType = typeof(SceneView).Assembly.GetType("UnityEditor.ConsoleWindow", throwOnError: false);
+            if (consoleWindowType == null) return;
+            
+            var consoleWindow = EditorWindow.GetWindow(consoleWindowType);
+            consoleWindow.Focus();
+        }
+    }
+}
