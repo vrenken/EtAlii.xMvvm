96028d90eaf64ec540e3be374a389d2081a96975
Peter Vrenken <github.com@petervrenken.nl>
2020-04-01 22:49:47 +0200

    Added comments

--- Playground/1. Structure/xMvvm.Unity/Assets/EtAlii/xMvvm/Editor/XamlVariant1/Templates/String.Extensions.cs
+++ Playground/1. Structure/xMvvm.Unity/Assets/EtAlii/xMvvm/Editor/XamlVariant1/Templates/String.Extensions.cs
@@ -1,7 +1,16 @@
 ﻿namespace EtAlii.xMvvm
 {
+	/// <summary>
+	/// A class where we can put simple string extensions.
+	/// Remark: These should be simple, and always completely string oriented (i.e. not related to other objects).
+	/// </summary>
 	public static class StringExtensions 
     {
+	    /// <summary>
+	    /// Convert the specified string into pascal case.
+	    /// </summary>
+	    /// <param name="input"></param>
+	    /// <returns></returns>
 	    public static string PascalCase(this string input)
 	    {
 		    if (string.IsNullOrEmpty(input))
